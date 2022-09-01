bc66657c353105d2c5229d0b6d593c0b1efb04d7
hiimhl <gofls5126@naver.com>
2022-09-01 23:44:47 +0900

    Button - type css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,36 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
-import RouteTest from "./components/RouteTest";
+import MyButton from "./components/MyButton";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">hi</div>
-      <RouteTest />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/edit" element={<Edit />} />
-        <Route path="/diary/:id" element={<Diary />} />
-      </Routes>
+      <div className="App">
+        <h2>App</h2>
+        <MyButton
+          text={"클릭하시오"}
+          onClick={() => alert("클릭감지")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"클릭하시오"}
+          onClick={() => alert("클릭감지")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"클릭하시오"}
+          onClick={() => alert("클릭감지")}
+          type={"dfdfdf"}
+        />
+        <img src={process.env.PUBLIC_URL + `./assets/emotion1.png`} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,21 @@
+//
+function MyButton(props) {
+  const btnType = ["positive", "negative"].includes(props.type)
+    ? props.type
+    : "default";
+  //버튼의 타입이 지정된 타입이 아니라면 강제로 dufault로 변경
+
+  return (
+    <button
+      className={["MyButton", `MyButton_${btnType}`].join(" ")}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </button>
+  );
+}
+
+MyButton.defaultProps = {
+  type: "default",
+};
+export default MyButton;
